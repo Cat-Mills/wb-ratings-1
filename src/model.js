4fe3114dcbbb4971d123900aed8fcb178f2e79f7
Cat-Mills <141058467+Cat-Mills@users.noreply.github.com>
2023-08-30 15:44:06 -0600

    Movie Model. Don't even get me STARTED on models in the film industry.

--- src/model.js
+++ src/model.js
@@ -1,4 +1,4 @@
-import { DataTypes, Model } from 'sequelize';
+import { DataTypes, INTEGER, Model, STRING, TEXT } from 'sequelize';
 import util from 'util';
 import connectToDB from './db.js';
 
@@ -32,3 +32,36 @@ User.init(
     sequelize: db,
   },
 );
+
+export class Movie extends Model {
+  [util.inspect.custom]() {
+    return this.toJSON();
+  }
+}
+
+Movie.init(
+  {
+    movie_id:{
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    overview:{
+      type: DataTypes.TEXT,
+    },
+    release_date:{
+      type: DataTypes.DATE
+    },
+    poster_path:{
+      type: DataTypes.STRING
+    },
+  },
+  {
+    modelName: 'movie',
+    sequelize: db,
+  },
+)
